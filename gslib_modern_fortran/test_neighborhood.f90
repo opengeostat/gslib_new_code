7e6b98372368ce70582c6d0a66e16fe218fc3204
Adrian Martinez Vargas <adriangeologo@yahoo.es>
2022-03-28 20:18:02 -0400

    minor updates

--- gslib_modern_fortran/test_neighborhood.f90
+++ gslib_modern_fortran/test_neighborhood.f90
@@ -15,6 +15,10 @@ program test_neighborhood
     real :: x(5) = [1,2,3,4,5]
     real :: y(5) = [1,2,3,4,5]
     real :: z(5) = [0,0,0,0,0]
+    real :: x0 = 1., y0 = 1., z0 =1., r =2.
 
-    print *,  ball (x, y, z, x0 = 1., y0 = 1., z0 =1., r =2.)
+    print *,  ball (x, y, z, x0, y0, z0, r)
+    print *,  ball (x0, y0, z0, x, y, z, r)
+    print *,  ball (x, y, z, x, y, z, r)
+    print *,  ball (x, y, z, x, y, z, x)
 end program
